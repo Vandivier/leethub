7dcda81d36865f62162cc1f505ffe0fc509b903c
John Vandivier <vandivier_john@yahoo.com>
2022-02-23 12:19:37 -0500

    Time: 433 ms (27.26%), Space: 49.7 MB (14.15%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -1,24 +1,3 @@
-//     def mergeTwoLists(self, l1, l2):
-//         prehead = ListNode(-1)
-
-//         prev = prehead
-//         while l1 and l2:
-//             if l1.val <= l2.val:
-//                 prev.next = l1
-//                 l1 = l1.next
-//             else:
-//                 prev.next = l2
-//                 l2 = l2.next            
-//             prev = prev.next
-
-//         # At least one of l1 and l2 can still have nodes at this point, so connect
-//         # the non-null list to the end of the merged list.
-//         prev.next = l1 if l1 is not None else l2
-
-//         return prehead.next
-
-
-
 const mergeSortTwoLists = (l1, l2) => {
     const head = new ListNode(-Infinity)
     let pointer = head;
