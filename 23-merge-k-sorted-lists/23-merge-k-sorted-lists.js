c4c5a28d2db1c629edcc00801163878c27d0ffe1
John Vandivier <vandivier_john@yahoo.com>
2022-02-24 19:50:49 -0500

    Time: 113 ms (81.81%), Space: 49.7 MB (15.05%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -1,29 +1,29 @@
-// const mergeSortTwoLists = (l1, l2) => {
-//     const head = new ListNode(-Infinity)
-//     let pointer = head;
+const mergeSortTwoLists = (l1, l2) => {
+    const head = new ListNode(-Infinity)
+    let pointer = head;
 
-//     while (l1 && l2) {
-//         if (l1.val <= l2.val) {
-//             // or, pointer.next = new ListNode(l1.val);
-//             pointer.next = l1;
-//             l1 = l1.next;
-//         } else {
-//             pointer.next = l2;
-//             l2 = l2.next;
-//         }
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            // or, pointer.next = new ListNode(l1.val);
+            pointer.next = l1;
+            l1 = l1.next;
+        } else {
+            pointer.next = l2;
+            l2 = l2.next;
+        }
 
-//         pointer = pointer.next;
-//     }
+        pointer = pointer.next;
+    }
 
-//     // we could be at bottom of one list but not other
-//     if (l1) {
-//         pointer.next = l1;
-//     } else {
-//         pointer.next = l2;
-//     }
+    // we could be at bottom of one list but not other
+    if (l1) {
+        pointer.next = l1;
+    } else {
+        pointer.next = l2;
+    }
 
-//     return head.next
-// }
+    return head.next
+}
 
 /**
  * Definition for singly-linked list.
@@ -55,9 +55,9 @@
 
 // binary interval merge / divide and conquer without splicing
 // (similar to official python solution)
-var mergeKListsIntervalMergeNoSplice = function(lists) {
+// var mergeKListsIntervalMergeNoSplice = function(lists) {
 // var mergeKLists = function(lists) {
-// const mergeKLists = (lists) => {
+const mergeKLists = (lists) => {
     let interval = 1;
 
     while (lists.length > interval) {
@@ -70,42 +70,3 @@ var mergeKListsIntervalMergeNoSplice = function(lists) {
 
     return lists[0] || null;
 };
-
-
-const mergeSort = (n1, n2) => {
-    const head = new ListNode(-Infinity);
-    let pointer = head;
-
-    // loop through both lists until first end
-    while (n1 && n2) {
-        if (n1.val <= n2.val) {
-            pointer.next = n1;
-            n1 = n1.next;
-        } else {
-            pointer.next = n2;
-            n2 = n2.next;
-        }
-
-        pointer = pointer.next;
-    }
-
-    // handle remaining tail
-    if (n1) {
-        pointer.next = n1;
-    } else {
-        pointer.next = n2;
-    }
-    
-    return head.next;
-}
-
-const mergeKLists = lists => {
-    // pop out a list each iter and merge to base
-    // loop until only base remains
-    while (lists.length > 1) {
-        const toMerge = lists.splice(1,1);
-        lists[0] = mergeSort(lists[0], toMerge[0])
-    }
-
-    return lists[0] || null;
-}
